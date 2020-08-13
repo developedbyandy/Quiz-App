731aeb4277540851356c8101daed4d6f6e844aa4
Andy Charles <66924525+developedbyandy@users.noreply.github.com>
2020-08-13 08:04:23 -0400

    Quiz App Assignment

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
+'use strict';
+//let counter = 0;
 /**
- * Example store structure
- */
-
-
-/**
+ * 
  * 
  * Technical requirements:
  * 
@@ -17,15 +15,181 @@
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
  * 
  */
-
 /********** TEMPLATE GENERATION FUNCTIONS **********/
-
 // These functions return HTML templates
+function generateHomePageHTML() {
+  return `    
+      <div class="container">
+                <h1>Welcome to the sports quiz!</h1>
+                <form id="js-opening-page">
+                    <button type="button" id="start-quiz">Start the Quiz!</button>
+                </form>
+            </div>`;
+  console.log('generateHomePageHTML() ran');
+}
+// this needs to reference our questions in our store object array
+function generateQuestionHTML() {
+  // store.questions[counter] will give the question at counter value
 
+  let question = store.questions[store.questionNumber];
+
+  return `
+  <div class="container">
+       <h1>${question.name}</h1>
+       <form id="js-question-form">
+           <input type="radio" id="male" name="answers" value=${question.answers[0]}>
+           <label for="male">${question.answers[0]}</label><br>
+           <input type="radio" id="female" name="answers" value=${question.answers[1]}>
+           <label for="female">${question.answers[1]}</label><br>
+           <input type="radio" id="other" name="answers" value=${question.answers[2]}>
+           <label for="other">${question.answers[2]}</label><br>
+           <input type="radio" id="other" name="answers" value=${question.answers[3]}>
+           <label for="other">${question.answers[3]}</label><br>
+           <button type="submit" id="give-answer">Send it!</button>
+       </form>
+       <quiz-place>Question number: ${store.questionNumber} out of 5</quiz-place><br>
+       <score-spot>You have gotten ${store.score} correct</score-spot>
+     </div>`;
+}
 /********** RENDER FUNCTION(S) **********/
-
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+// need to alert when user enters answer right or wrong.
+function renderMain() {
+  if (store.quizStarted) {
+    renderQuestionPage();
+  } else {
+    renderHomePage();
+  }
+  //if quiz started true render question page else render home
+  // if quiz started true render question
+  // as long as questions.length
+  //
+}
 
+
+
+
+
+
+function renderHomePage() {
+
+  let homePage = generateHomePageHTML();
+  $('main').html(homePage);
+
+  console.log('renderHomePage() ran');
+}
+
+
+function renderQuestionPage() {
+  // How do we keep track of questions and keep going through our array of objects(questions).
+  // if Quiz start: true
+  let questionHTML = generateQuestionHTML();
+
+  $('main').html(questionHTML);
+  console.log('renderQuestionPage() ran');
+}
+
+function renderResultsPage() {
+  let resultsHTML = generateResultsPageHTML();
+  $('main').html(resultsHTML)
+
+}
 /********** EVENT HANDLER FUNCTIONS **********/
-
 // These functions handle events (submit, click, etc)
+// This needs to send us to our question page.
+
+function startPageButton() {
+  $('#start-quiz').on('click', event => {
+    event.preventDefault();
+    renderQuestionPage();
+
+    console.log(store.questionNumber);
+
+  });
+  console.log('startPageButton() ran');
+}
+// submitAnswer needs to send us to our next question page, by adding to the counter value.
+// also needs to compare the correct answer
+// access correct answer with store.questions[0].correctAnswer
+function submitAnswer() {
+
+  $('main').on('submit', '#js-question-form', function (event) {
+    event.preventDefault();
+    let question = store.questions[store.questionNumber];
+    //needs an if statement only increment as long as store.questions.length -1
+    // also need
+
+
+    if (parseInt(event.target.answers.value) === question.correctAnswer) {
+      store.score++;
+    } else {
+
+      alert(`Sorry, wrong Answer the correct answer was ${question.correctAnswer}`);
+    }
+
+    store.questionNumber += 1;
+    if (store.questionNumber < 5) {
+      renderQuestionPage();
+    } else if (store.questionNumber = 5) {
+      renderResultsPage();
+    } else {
+      renderHomePage();
+    }
+
+
+  });
+  // needs to match 
+
+
+
+}
+
+function collectAnswers() {
+  $('input[type=radio').click(function (event) {
+    console.log('blah blah', $('input[name=answers]:checked').val());
+  })
+
+}
+
+function resultsPageButton() {
+  $("return-home").on("click", "js-results-page", function (event) {
+    event.preventDefault();
+    renderHomePage();
+
+    console.log("blach blh");
+
+  });
+
+}
+
+
+function generateResultsPageHTML() {
+  return `    
+  <div class="container">
+            <h1>Welcome to the sports quiz!</h1>
+            <form id="js-results-page">
+                <button type="button" id="return-home">Try Again!</button>
+            </form>
+            <p>Congrats! You got ${store.score} correct</p>
+        </div>`;
+  console.log('generateHomePageHTML() ran');
+
+}
+
+
+
+function handleQuiz() {
+  // needs to render our home page, and activate all of our other functions.
+  // console.log(questions.length);
+  renderMain();
+  generateHomePageHTML();
+  //renderHomePage();
+  generateQuestionHTML();
+  //renderQuestionPage();
+  startPageButton();
+  submitAnswer();
+  collectAnswers();
+  resultsPageButton();
+  console.log('handleQuiz() ran');
+}
+$(handleQuiz);
--- store.js
+++ store.js
@@ -1,70 +1,79 @@
+'use strict';
+
+
 const store = {
-    // 5 or more questions are required
-    questions: [
-      {
-        question: 'How many points are awarded for a touchdown in NFL football?',
-        answers: [
-          1,
-          2,
-          3,
-          6,
-        ],
-        correctAnswer: 6,
-      },
-      {
-        question: 'How many points are awarded for a layup in NBA basketball?',
-        answers: [
-          1,
-          2,
-          3,
-          4,
-        ],
-        correctAnswer: 2,
-      },
-      {       
-          question: 'How many points are awarded for a fieldgoal in NFL football?'
-          answers: [
-              1,
-              2,
-              3,
-              6,
-          ],
-          correctAnswer: 3,
-      }
-      {
-          question: 'How many minutes are in an NBA quarter?'
-          answers: [
-              '12 minutes',
-              '6 minutes',
-              '30 minutes',
-              '1 hour',
-
-          ],
-          correctAnswer: '12 minutes',
-      }
-      {
-          question: 'How many points are awarded for returning a missed point after attempt to the opposing teams enzone',
-          answers: [
-              1,
-              2,
-              3,
-              6,
-          ],
-          correctAnswer: 2,
-      }
-      {
-          question: 'How many quarters are in an NBA game?'
-          answers: [
-              4,
-              6,
-              2,
-              3,
-          ],
-          correctAnswer: 4,
-      }
+  // 5 or more questions are required
+  questions: [{
+    name: 'How many points are awarded for a touchdown in NFL football?',
+    answers: [
+      1,
+      2,
+      3,
+      6,
     ],
+    correctAnswer: 6,
+  },
+  {
+    name: 'How many points are awarded for a layup in NBA basketball?',
+    answers: [
+      1,
+      2,
+      3,
+      4,
+    ],
+    correctAnswer: 2,
+  },
+  {
+    name: 'How many points are awarded for a fieldgoal in NFL football?',
+    answers: [
+      1,
+      2,
+      3,
+      6,
+    ],
+    correctAnswer: 3,
+  },
+  {
+    name: 'How many minutes are in an NBA quarter?',
+    answers: [
+      12,
+      6,
+      30,
+      60,
+    ],
+    correctAnswer: 12,
+  },
+  {
+    name: 'How many points are awarded for returning a missed point after attempt to the opposing teams endzone',
+    answers: [
+      1,
+      2,
+      3,
+      6,
+    ],
+    correctAnswer: 2,
+  },
+  {
+    name: 'How many quarters are in an NBA game',
+    answers: [
+      4,
+      6,
+      2,
+      3,
+    ],
+    correctAnswer: 4
+  },
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
+let currentQuestion = store.questions[store.questionCount];
 
-    quizStarted: false,
-    questionNumber: 0,
-    score: 0
-  };
+
+console.log(store.questions[0].name);
+console.log(questions.name);
+console.log(store.questions[store.questionNumber]);
+
+
+
